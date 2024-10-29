2a3060ad60ef8cf8c516c71afa1eb86a4ed51f38
m.moiseev <iammoiseev@gmail.com>
2024-10-29 19:22:53 +0300

    oo

--- src/main/java/org/vimpelcom/Main.java
+++ src/main/java/org/vimpelcom/Main.java
@@ -3,5 +3,6 @@ package org.vimpelcom;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, EBAT!");
+        System.out.println("Hello, EBAT!");
     }
 }
